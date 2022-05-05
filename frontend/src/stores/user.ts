7a6920da4be12ea0e2d22235fa706bad3b20c188
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-05 11:11:34 +0530

    load profile with user

--- frontend/src/stores/user.ts
+++ frontend/src/stores/user.ts
@@ -6,12 +6,27 @@ import appwrite from '../lib/appwrite'
 // and only the whole object gets changed at a time
 export const user = atom<Models.User<Models.Preferences> | null>(null)
 
+interface Profile {
+	$id: string
+	avatar_url: string
+	bio: string | null
+	status: string
+}
+
+export const profile = atom<Profile | null>(null)
+
 export const loadUser = action(user, 'loadUser', async () => {
 	let u = null
 	try {
 		u = await appwrite.account.get()
 	} catch {}
 	user.set(u)
+
+	if (u) {
+		// load profile
+		const p = await appwrite.database.getDocument('profiles', u.$id)
+		profile.set(p as any)
+	} else profile.set(null)
 })
 
 export default user
