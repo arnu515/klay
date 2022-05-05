4c23f909bce9f43587dc2662b544eee723878619
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-05 10:48:20 +0530

    display toolbar title instead of hardcoded value

--- frontend/src/layouts/SettingsLayout.vue
+++ frontend/src/layouts/SettingsLayout.vue
@@ -1,6 +1,13 @@
 <template>
 	<div style="padding: 1rem">
-		<h1 class="text-h4">Settings</h1>
+		<h1 class="text-h4">{{ title }}</h1>
 	</div>
 	<router-view />
 </template>
+
+<script lang="ts" setup>
+import { useStore } from '@nanostores/vue'
+import tt from '../stores/toolbarTitle'
+
+const title = useStore(tt)
+</script>
