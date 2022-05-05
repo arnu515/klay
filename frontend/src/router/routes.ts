7cc5b8ebdb6894c1f0a748bcd6bef92e821264c6
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-05 10:23:49 +0530

    init settings page

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
 	{
 		path: '/',
 		component: () => import('layouts/MainLayout.vue'),
-		children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+		children: [
+			{ path: '', component: () => import('pages/IndexPage.vue') },
+			{ path: 'settings', component: () => import('pages/SettingsPage.vue') }
+		]
 	},
 
 	{
