c20c251939c92c5cdefbacb9e15be459651d3957
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-05 15:26:41 +0530

    add method for updating avatar

--- backend/src/routers/profile.py
+++ backend/src/routers/profile.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Depends, Body
+import os
+from PIL import Image
+
+from appwrite.exception import AppwriteException
+from fastapi import APIRouter, Depends, Body, UploadFile
 from pydantic import BaseModel, Field
 
+from src.exceptions.HTTPException import HTTPException
 from src.util.aw import Appwrite, get_admin_appwrite
 from src.dependencies.auth import auth
 
@@ -35,6 +40,38 @@ def update_my_profile(body: UpdateProfileBody = Body(...), user: Appwrite = Depe
     profile = user.database.get_document("profiles", user.account.get().get("$id"))
     profile["status"] = body.status.strip()
     profile["bio"] = body.bio.strip() if body.bio else None
+    # update profile
+    appwrite.database.update_document("profiles", profile.get("$id"), profile)
+    return profile
+
+
+@router.put("/me/avatar")
+def change_my_avatar(file: UploadFile, user: Appwrite = Depends(auth()),
+                  appwrite: Appwrite = Depends(get_admin_appwrite)):
+    """
+    Change current user's avatar
+    """
+    # check file type
+    if file.content_type not in ["image/jpeg", "image/png"]:
+        raise HTTPException("Invalid file type. Must be png or jpeg", 422)
+
+    # get profile from collection
+    profile = user.database.get_document("profiles", user.account.get().get("$id"))
+    with open("avatar.png", "wb") as f:
+        f.write(file.file.read())
+        print("saved file")
+    # resize image to 64x64px
+    img = Image.open("avatar.png")
+    img.thumbnail((128, 128))
+    img.save("avatar.png")
+    # upload image to storage
+    try:
+        appwrite.storage.create_file('avatars', profile.get("$id"), "avatar.png")
+        appwrite.storage.update_file('avatars', profile.get("$id"))
+    except AppwriteException as e:
+        raise HTTPException(e.message, e.code)
+    os.remove("avatar.png")
+    profile["avatar_url"] = f"{os.getenv('APPWRITE_ENDPOINT')}/v1/storage/buckets/avatars/files/{profile.get('$id')}/view?project={os.getenv('APPWRITE_PROJECT_ID')}"
     # update profile
     appwrite.database.update_document("profiles", profile.get("$id"), profile)
     return profile
