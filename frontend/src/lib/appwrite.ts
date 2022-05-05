59e78d3bf4d835d313f4a56256f89cc0eb09b472
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-05 12:44:41 +0530

    put appwrite in window if development

--- frontend/src/lib/appwrite.ts
+++ frontend/src/lib/appwrite.ts
@@ -6,4 +6,6 @@ appwrite
 	.setEndpoint(process.env.APPWRITE_ENDPOINT!)
 	.setProject(process.env.APPWRITE_PROJECT_ID!)
 
+if (process.env.NODE_ENV === 'development') (window as any)['appwrite'] = appwrite
+
 export default appwrite
