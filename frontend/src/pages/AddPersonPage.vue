99b4ca136136c0dfa22f98cfd8c8dccf085a6d3f
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-06 10:31:08 +0530

    add qr code scan

--- frontend/src/pages/AddPersonPage.vue
+++ frontend/src/pages/AddPersonPage.vue
@@ -1,11 +1,50 @@
 <template>
-	<main>
+	<main v-if="!isScanningQR">
 		<div class="wrapper">
-			<q-card flat bordered style="height: 100%" class="message-box">
-				<h1 class="text-h4 q-px-md">Add a person</h1>
+			<q-card flat bordered style="height: 100%" class="q-py-sm q-px-md">
+				<h1 class="text-h4">Add a person</h1>
+				<q-form @submit.prevent="addPerson">
+					<h3 class="text-h6">Enter a person's ID to add them</h3>
+					<q-input
+						outlined
+						bottom-slots
+						v-model="personId"
+						aria-label="Person ID"
+						placeholder="Enter person's ID"
+						clearable
+					>
+						<template v-slot:append v-if="showClipboardButton">
+							<q-btn
+								flat
+								dense
+								title="Paste from clipboard"
+								aria-label="Paste from clipboard"
+								@click="pasteFromClipboard"
+							>
+								<q-icon name="content_paste" />
+							</q-btn>
+						</template>
+						<template v-slot:after v-if="showQRButton">
+							<q-btn
+								color="primary"
+								title="Scan QR Code"
+								aria-label="Scan QR Code"
+								round
+								@click="scanQRCode"
+							>
+								<q-icon name="qr_code_scanner" />
+							</q-btn>
+						</template>
+					</q-input>
+				</q-form>
 			</q-card>
 		</div>
 	</main>
+	<ScanQRCode
+		v-else
+		@close="isScanningQR = false"
+		@scan="res => (personId = res.content)"
+	/>
 </template>
 
 <style lang="scss" scoped>
@@ -25,3 +64,59 @@ main {
 	height: 100%;
 }
 </style>
+
+<script lang="ts" setup>
+import { Dialog, useQuasar } from 'quasar'
+import { onMounted, ref } from 'vue'
+import { Clipboard } from 'app/src-capacitor/node_modules/@capacitor/clipboard'
+import ScanQRCode from 'components/ScanQRCode.vue'
+
+const personId = ref('')
+const showClipboardButton = ref(true)
+const showQRButton = ref(true)
+const isScanningQR = ref(false)
+const q = useQuasar()
+
+onMounted(() => {
+	if (!q.platform.is.capacitor && typeof navigator.clipboard.readText !== 'function') {
+		showClipboardButton.value = false
+	}
+})
+
+async function scanQRCode() {
+	isScanningQR.value = true
+}
+
+function addPerson() {
+	//
+}
+
+function pasteFromClipboard() {
+	if (q.platform.is.capacitor) {
+		console.log(Clipboard)
+		Clipboard.read()
+			.then(text => {
+				console.log(text)
+				personId.value = text.value
+			})
+			.catch(e => {
+				Dialog.create({
+					title: 'Error',
+					message: "Couldn't paste text from your clipboard"
+				})
+				throw e
+			})
+	} else
+		navigator.clipboard
+			.readText()
+			.then(text => {
+				personId.value = text
+			})
+			.catch(() => {
+				Dialog.create({
+					title: 'Error',
+					message: "Couldn't paste text from your clipboard"
+				})
+			})
+}
+</script>
