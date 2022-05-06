0885e7a61a768fac068f42b3732b7d121e615cdb
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-06 12:06:54 +0530

    show user's ID

--- frontend/src/pages/AddPersonPage.vue
+++ frontend/src/pages/AddPersonPage.vue
@@ -36,7 +36,37 @@
 							</q-btn>
 						</template>
 					</q-input>
+					<div class="flex justify-end">
+						<q-btn color="primary" type="submit">Add person</q-btn>
+					</div>
 				</q-form>
+				<br />
+				<h3 class="text-h6">Your ID</h3>
+				<q-field filled>
+					<template v-slot:control>
+						<div class="self-center full-width no-outline" tabindex="0">
+							{{ user.$id }}
+						</div>
+					</template>
+					<template v-slot:after>
+						<q-btn
+							color="primary"
+							title="Copy"
+							aria-label="Copy ID"
+							round
+							@click="copyID"
+						>
+							<q-icon name="content_copy" />
+						</q-btn>
+					</template>
+				</q-field>
+				<div class="qr-img">
+					<q-img
+						:src="userIdQrImage"
+						alt="QR Code"
+						style="width: 165px; height: 165px; border: 1px black solid"
+					/>
+				</div>
 			</q-card>
 		</div>
 	</main>
@@ -67,15 +97,34 @@ main {
 	margin: 1rem auto;
 	padding-bottom: 1rem;
 	height: 100%;
+	overflow: auto;
+	scrollbar-color: $primary white;
+	scrollbar-width: thin;
+	&::-webkit-scrollbar-thumb {
+		background-color: $primary;
+	}
+	&::-webkit-scrollbar {
+		width: 5px;
+		background-color: inherit;
+	}
+}
+.qr-img {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: 1rem;
 }
 </style>
 
 <script lang="ts" setup>
 import { Dialog, useQuasar } from 'quasar'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { Clipboard } from 'app/src-capacitor/node_modules/@capacitor/clipboard'
 import ScanQRCode from 'components/ScanQRCode.vue'
 import toolbarTitle from 'src/stores/toolbarTitle'
+import userStore from 'src/stores/user'
+import { useStore } from '@nanostores/vue'
+import appwrite from 'src/lib/appwrite'
 
 toolbarTitle.set('Add a person')
 const personId = ref('')
@@ -83,6 +132,10 @@ const showClipboardButton = ref(true)
 const showQRButton = ref(true)
 const isScanningQR = ref(false)
 const q = useQuasar()
+const user = useStore(userStore)
+const userIdQrImage = computed(() => {
+	return appwrite.avatars.getQR(user.value?.$id || 'loading', 256, 2).toString()
+})
 
 onMounted(() => {
 	if (
@@ -114,7 +167,6 @@ function addPerson() {
 
 function pasteFromClipboard() {
 	if (q.platform.is.capacitor) {
-		console.log(Clipboard)
 		Clipboard.read()
 			.then(text => {
 				console.log(text)
@@ -139,5 +191,36 @@ function pasteFromClipboard() {
 					message: "Couldn't paste text from your clipboard"
 				})
 			})
+}
+
+function copyID() {
+	function d() {
+		Dialog.create({
+			title: 'Copied to clipboard',
+			message: 'Copied your ID to your clipboard'
+		})
+	}
+	if (q.platform.is.capacitor) {
+		Clipboard.write({
+			string: user.value!.$id,
+			label: q.platform.is.android ? 'Your Klay ID' : undefined
+		}).then(d)
+	} else {
+		if (
+			'clipboard' in navigator &&
+			typeof navigator.clipboard.writeText === 'function'
+		) {
+			navigator.clipboard.writeText(user.value!.$id)
+		} else {
+			// use old method
+			const textarea = document.createElement('textarea')
+			textarea.value = user.value!.$id
+			document.body.appendChild(textarea)
+			textarea.select()
+			document.execCommand('copy')
+			document.body.removeChild(textarea)
+		}
+		d()
+	}
 }
 </script>
