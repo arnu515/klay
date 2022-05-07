059f54ea1a6f583694433c316756f0a43efa1eab
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-07 19:21:35 +0530

    use python dotenv in main.py to load env

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
-import os
+from dotenv.main import DotEnv
 
 import uvicorn
 
+
 if __name__ == "__main__":
+    env = DotEnv(".env").dict()
     uvicorn.run(
         "src:app",
-        host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", "5000")),
-        reload=bool(int(os.getenv("DEV", "1"))),
+        host=env.get("HOST", "0.0.0.0"),
+        port=int(env.get("PORT", "5000")),
+        reload=bool(int(env.get("DEV", "1"))),
         env_file=".env"
     )
