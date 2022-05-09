e5554ec9b4bb4bfb0f5c76b34fdb52e1f4f0e340
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-09 19:38:14 +0530

    use correct env var

--- backend/src/util/mongo.py
+++ backend/src/util/mongo.py
@@ -2,6 +2,6 @@ import os
 
 from pymongo import MongoClient
 
-mongo = MongoClient(os.environ.get("MONGO_URI"))
+mongo = MongoClient(os.environ.get("MONGODB_URI"))
 
 db = mongo[os.environ.get("MONGODB_DB", "klay")]
