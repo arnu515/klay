d9f53ab3ede6318b7323a230ccb4f05df4bbf2c1
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-10 21:03:59 +0530

    listen to events

--- frontend/src/stores/messages.ts
+++ frontend/src/stores/messages.ts
@@ -1,7 +1,11 @@
-import { map } from 'nanostores'
+import { map, onMount } from 'nanostores'
+import { Dialog, Notify } from 'quasar'
+import appwrite from 'src/lib/appwrite'
+import axios from 'src/lib/axios'
 import { loadMessages } from 'src/lib/cache/messages'
 import { getProfile } from 'src/lib/cache/profile'
-import type { Message as Msg } from 'src/lib/types'
+import type { ChatEvent, Message as Msg } from 'src/lib/types'
+import { parseFastApiError } from 'src/lib/util'
 import user from './user'
 
 export interface MessageItem {
@@ -16,6 +20,58 @@ export interface MessageItem {
 
 export const messages = map<Record<string, MessageItem[]>>({})
 
+onMount(messages, () => {
+	console.log('[s] subscribed to store')
+	const unsub = appwrite.subscribe<ChatEvent>(
+		'collections.chat_events.documents',
+		async doc => {
+			console.log(doc)
+			const u = user.get()
+			if (!u) return
+			if (doc.payload.userId2 !== u.$id) return
+			const data = doc.payload
+			if (!doc.event.includes('create')) return
+
+			switch (data.type) {
+				case 'create': {
+					const res = await axios.get<{ message: Msg }>(
+						'/api/chat_messages/message-' + data.messageId
+					)
+					if (res.status !== 200) {
+						Dialog.create({
+							title: 'An error occured',
+							message: parseFastApiError(res.data)
+						})
+						return
+					}
+					await MessageMethods.create(doc.payload.userId1, res.data.message)
+				}
+				case 'update': {
+					const res = await axios.get<{ message: Msg }>(
+						'/api/chat_messages/message-' + data.messageId
+					)
+					if (res.status !== 200) {
+						Dialog.create({
+							title: 'An error occured',
+							message: parseFastApiError(res.data)
+						})
+						return
+					}
+					await MessageMethods.update(doc.payload.userId1, res.data.message)
+				}
+				case 'delete': {
+					await MessageMethods.delete(doc.payload.userId1, data.messageId)
+				}
+			}
+		}
+	)
+
+	return () => {
+		console.log('[s] unsubscribed from store')
+		unsub()
+	}
+})
+
 export async function getMessages(userId: string) {
 	const msgs = await loadMessages(userId)
 	if (!msgs.length) return []
@@ -29,6 +85,42 @@ export async function getMessages(userId: string) {
 
 	messages.setKey(userId, items)
 	return items
+}
+
+export class MessageMethods {
+	static async create(userId: string, msg: Msg, showNotif = true) {
+		const m = messages.get()[userId]
+		console.log({ m })
+		if (!m) return
+		const message = await Message(msg)
+		messages.setKey(userId, [...m, message])
+
+		if (showNotif) {
+			Notify.create({
+				message: 'New message from ' + message.name
+			})
+		}
+	}
+
+	static async update(userId: string, msg: Msg) {
+		const m = messages.get()[userId]
+		console.log({ m })
+		if (!m) return
+		messages.setKey(
+			userId,
+			await Promise.all(m.map(async m => (m.id === msg._id ? await Message(msg) : m)))
+		)
+	}
+
+	static async delete(userId: string, msgId: string) {
+		const m = messages.get()[userId]
+		console.log({ m })
+		if (!m) return
+		messages.setKey(
+			userId,
+			m.filter(m => m.id !== msgId)
+		)
+	}
 }
 
 export async function Message(message: Msg) {
