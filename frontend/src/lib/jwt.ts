16a5781d77b3b6bd7e0d4351cae40480b91a95db
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-11 18:47:26 +0530

    use localstorage

--- frontend/src/lib/jwt.ts
+++ frontend/src/lib/jwt.ts
@@ -5,16 +5,17 @@
 
 import appwrite from './appwrite'
 
-export let jwt = ''
-export let jwtCreatedAt = 0
-
 export async function getJWT() {
 	try {
+		let jwt = localStorage.getItem('jwt')
+		let jwtCreatedAt = parseInt(localStorage.getItem('jwtCreatedAt') || '0')
 		if (jwt && jwtCreatedAt + 15 * 60 * 1000 > Date.now()) {
 			return jwt
 		}
 		jwt = (await appwrite.account.createJWT()).jwt
 		jwtCreatedAt = Date.now()
+		localStorage.setItem('jwt', jwt)
+		localStorage.setItem('jwtCreatedAt', jwtCreatedAt.toString())
 		return jwt
 	} catch {
 		return ''
