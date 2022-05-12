798164d082002711a5b217ab9abe874ce2f72c05
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-12 11:44:53 +0530

    convert to cache from store

--- frontend/src/lib/cache/publicKeys.ts
+++ frontend/src/lib/cache/publicKeys.ts
@@ -1,14 +1,15 @@
+import { InMemoryCache } from '.'
 import { Models } from 'appwrite'
-import { map } from 'nanostores'
 import appwrite from 'src/lib/appwrite'
 
-export const keys = map<Record<string, string>>({})
+export const keys = new InMemoryCache('keys', '1 hour')
 
-export async function loadKey(userId: string) {
+export async function loadKey(userId: string, force = false) {
+	if (!force && keys.get(userId)) return keys.get(userId)
 	const { public: publicKey } = await appwrite.database.getDocument<
 		Models.Document & { public: string }
 	>('chat_keys', userId)
-	keys.setKey(userId, publicKey)
+	keys.set(userId, publicKey)
 	return publicKey
 }
 
