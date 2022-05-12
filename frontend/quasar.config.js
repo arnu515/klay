27dcc32efdc5d8dd8a863aaa16d6b65f576f262c
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-05-12 10:28:54 +0530

    add bottomsheet

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -101,7 +101,7 @@ module.exports = configure(function (ctx) {
 			// directives: [],
 
 			// Quasar plugins
-			plugins: ['Dialog', 'Notify']
+			plugins: ['Dialog', 'Notify', 'BottomSheet']
 		},
 
 		// animations: 'all', // --- includes all animations
